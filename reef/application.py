409509a41eaa83006b419f07b2387435e127cb06
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-11-01 11:49:28 +0530

    Add temporary changes

--- reef/application.py
+++ reef/application.py
@@ -1,8 +1,5 @@
 from flask import Flask, escape, request
 import requests
-import pytz
-import functools
-
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
@@ -17,7 +14,63 @@ class HttpStatusException(Exception):
         super().__init__(*args, **kwargs)
 
     def __str__(self):
-        return"{}({}, {}, {})".format(self.__class__.__name__, self.url, self.status_code, self.reason)
+        return "{}({}, {}, {})".format(
+            self.__class__.__name__, self.url, self.status_code, self.reason
+        )
+
+
+class User:
+    def __init__(self, email=EMAIL, password=PASSWORD, app_token=APP_TOKEN):
+        self.email = email
+        self.password = password
+        self.app_token = app_token
+        # self.org_ids = self.organization_ids()
+
+    @property
+    def auth_token(self):
+        params = {"email": self.email, "password": self.password}
+        response = requests.post(
+            AUTH_ENDPOINT, params=params, headers={"App-Token": self.app_token}
+        )
+        # TODO: Error handling
+        return response.json()["user"]["auth_token"]()
+
+    def organization_ids(self):
+        """
+        Retrieves organization ids for the authenticated user.
+        :param auth_token:
+        :return: List of organization ids.
+        """
+        response = requests.get(
+            ORGS_ENDPOINT,
+            headers={"App-Token": APP_TOKEN, "Auth-Token": self.auth_token},
+        )
+        return [org["id"] for org in response.json().get("organizations")]
+
+    def project_ids(self):
+        org_id = self.organization_ids()[0]
+        return retrieve_user_project_ids(self.auth_token, org_id)
+
+    def organization_user_ids(self):
+        org_id = self.organization_ids()[0]
+        return retrieve_organization_members(self.auth_token, org_id)
+
+    def reports(self, date=None):
+        date = date or (datetime.now() + timedelta(days=-1)).strftime("%Y-%m-%d")
+        csv = lambda x: ",".join(map(str, list(x)))
+        return custom_by_member_report(
+            self.auth_token,
+            csv(self.organization_user_ids()),
+            csv(self.organization_ids()),
+            csv(self.project_ids()),
+            date,
+        )
+
+
+"""
+[{'id': 720879, 'name': 'Srinivas Reddy Thatiparthy', 'duration': 16626, 'dates': [{'date': '2019-10-28', 'duration': 3600, 'projects': [{'id': 811570, 'name': 'ProjectB', 'duration': 3600}]}, {'date': '2019-10-29', 'duration': 7200, 'projects': [{'id': 811568, 'name': 'hubstaff bot50', 'duration': 3600}, {'id': 811569, 'name': 'ProjectA', 'duration': 3600}]}, {'date': '2019-10-30', 'duration': 5826, 'projects': [{'id': 811568, 'name': 'hubstaff bot50', 'duration': 4811}, {'id': 811569, 'name': 'ProjectA', 'duration': 946}, {'id': 811570, 'name': 'ProjectB', 'duration': 69}]}]}]
+"""
+
 
 def retrieve_auth_token():
     params = {"email": EMAIL, "password": PASSWORD}
@@ -27,11 +80,17 @@ def retrieve_auth_token():
     if response.status_code == 401:
         raise HttpStatusException(AUTH_ENDPOINT, 401, "Invalid email and/or password")
     elif response.status_code == 403:
-        raise HttpStatusException(AUTH_ENDPOINT, 403, "API access is only for organizations on an active plan")
+        raise HttpStatusException(
+            AUTH_ENDPOINT, 403, "API access is only for organizations on an active plan"
+        )
     elif response.status_code == 404:
         raise HttpStatusException(AUTH_ENDPOINT, 404, "Could not find record")
     elif response.status_code == 429:
-        raise HttpStatusException(AUTH_ENDPOINT, 429, "Rate limit has been reached. Please wait before making your next request.")
+        raise HttpStatusException(
+            AUTH_ENDPOINT,
+            429,
+            "Rate limit has been reached. Please wait before making your next request.",
+        )
     return response.json()["user"]["auth_token"]
 
 
@@ -44,7 +103,6 @@ def retrieve_org_ids(auth_token):
     response = requests.get(
         ORGS_ENDPOINT, headers={"App-Token": APP_TOKEN, "Auth-Token": auth_token}
     )
-    print(response.json())
     return [org["id"] for org in response.json().get("organizations")]
 
 
@@ -78,41 +136,50 @@ def retrieve_organization_members(auth_token, org_id):
     return [org["id"] for org in response.json().get("users")]
 
 
-def custom_by_member_date_report(
-    auth_token, start_date, end_date, organizations, users, project_ids
+def custom_by_member_report(
+    auth_token,
+    users,
+    organizations,
+    project_ids,
+    date=(datetime.now() + timedelta(days=-4)).strftime("%Y-%m-%d"),
 ):
     response = requests.get(
-        CUSTOM_BY_MEMBER_ENDPOINT,
+        CUSTOM_BY_MEMBER_TEAM_ENDPOINT,
         params={
-            "start_date": start_date,
-            "end_date": end_date,
+            "start_date": date,
+            "end_date": datetime.now().strftime("%Y-%m-%d"),
             "organizations": organizations,
             "projects": project_ids,
             "users": users,
         },
         headers={"App-Token": APP_TOKEN, "Auth-Token": auth_token},
     )
-    print(response.json())
-    return [org["id"] for org in response.json().get("organizations")]
+    return response.json()
 
 
 @app.route("/reports", methods=["GET"])
 def reports():
-    auth_token = retrieve_auth_token()
+    user = User()
+    auth_token = user.auth_token
     org_ids = retrieve_org_ids(auth_token)
     users = set()
     for org_id in org_ids:
         users.update(set(retrieve_organization_members(auth_token, org_id)))
-    users = list(users)
     project_ids = set()
     for user in users:
         project_ids.update(set(retrieve_user_project_ids(auth_token, user)))
-    project_ids = list(project_ids)
-    start_date = (datetime.now() + timedelta(days=-200)).strftime("%Y-%m-%d")
-    end_date = (datetime.now() + timedelta(days=-10)).strftime("%Y-%m-%d")
-    result = custom_by_member_date_report(auth_token, start_date, end_date, org_ids, users, project_ids)
-    import pdb; pdb.set_trace()
+    cs_project_ids = ",".join(map(str, list(project_ids)))
+    cs_org_ids = ",".join(map(str, list(org_ids)))
+    cs_users = ",".join(map(str, list(users)))
+    report = custom_by_member_report(auth_token, cs_users, cs_org_ids, cs_project_ids)
+    pt_raw_data = []
+    for org in report.get("organizations"):
+        if org["id"] in org_ids:
+            pt_raw_data = org["users"]
+    import pdb
+
+    pdb.set_trace()
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- reef/config.py
+++ reef/config.py
@@ -8,7 +8,9 @@ ORGS_ENDPOINT = API_ENDPOINT + "/organizations"
 USERS_ENDPOINT = API_ENDPOINT + "/users"
 PROJECTS_ENDPOINT = API_ENDPOINT + "/users/{id}/projects"
 ORG_MEMBERS_ENDPOINT = API_ENDPOINT + "/organizations/{id}/members"
-CUSTOM_BY_MEMBER_ENDPOINT = API_ENDPOINT + "/weekly/team"
+CUSTOM_BY_MEMBER_TEAM_ENDPOINT = API_ENDPOINT + "/custom/by_member/team"
+
+
 TIMEZONE = "Europe/Warsaw"
 APP_TOKEN = os.environ.get("APP_TOKEN", "TAgq83AeDd6PKGu9hoB86JhTkaAkLqU3_tabEQmi3ws")
 FLASK_APP = os.environ.get("FLASK_APP", "application.py")
