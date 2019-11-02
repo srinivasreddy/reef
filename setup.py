4f81cab551c87fabac9f434550aaeb81f9be6f89
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-11-02 13:25:40 +0530

    i[dated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="reef",
-    version="0.0.1",
+    version="0.0.2",
     url="https://srinivasreddy.dev",
     license="BSD",
     maintainer="Srinivas Reddy Thatiparthy",
